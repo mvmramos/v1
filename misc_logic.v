f549029229a082a9ae6586941add63471070b955
mvmramos <marcus.ramos@univasf.edu.br>
2015-09-06 14:58:50 -0300

    Create misc_logic.v

---
+++ misc_logic.v
@@ -0,0 +1,36 @@
+(* ---------------------------------------------------------------------
+   This file contains definitions and proof scripts related to 
+   (i) closure operations for context-free grammars, 
+   (ii) context-free grammars simplification 
+   (iii) context-free grammar Chomsky normalization and 
+   (iv) pumping lemma for context-free languages.
+   
+   More information can be found in the paper "Formalization of the
+   pumpung lemma for context-free languages", submitted to
+   LFCS 2016.
+   
+   Marcus Vinícius Midena Ramos
+   mvmramos@gmail.com
+   --------------------------------------------------------------------- *)
+   
+(* --------------------------------------------------------------------- *)
+(* BASIC LEMMAS                                                          *)
+(* --------------------------------------------------------------------- *)
+
+Lemma sig_weak {A: Type} {P P': A -> Prop} (H: forall x: A, P x -> P' x) (a: {x | P x}): {x | P' x}.
+Proof.
+destruct a as [x H0].
+exists x.
+apply H.
+exact H0.
+Qed.
+
+Lemma contrap:
+forall P1 P2: Prop,
+(P1 -> P2) -> (~ P2 -> ~ P1).
+Proof.
+intros P1 P2 H1 H2 H3.
+apply H2.
+apply H1.
+exact H3.
+Qed.
